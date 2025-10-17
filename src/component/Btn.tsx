34715f2037821090f1c405fcac2faab1b14355f4
Nikkihosseini <niekal00@gmail.com>
2025-10-17 11:58:58 +0330

    feat: create Btn component with typed props

--- src/component/Btn.tsx
+++ src/component/Btn.tsx
@@ -1,11 +1,11 @@
+import type  { ButtonProps } from './types/types'
 
 
-
-export default function Btn({text}){
+export default function Btn({text}: ButtonProps){
     return(
         <>
-            <div>
-
+            <div className='flex items-center justify-center h-[2.7rem] w-40 rounded-[2.5rem] border-2 border-amber-400 p-2 text-amber-400 font-semibold uppercase text-sm cursor-pointer'>
+                { text }
             </div>
         </>
     )
