5644a94d9c3c220b3a06f12cbbe33b759add98d9
Nikkihosseini <niekal00@gmail.com>
2025-10-17 12:06:47 +0330

    feat: add Button and context for Home section

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,4 +1,5 @@
 import Header from '../component/Header'
+import Btn from '../component/Btn'
 
 
 
@@ -9,6 +10,22 @@ export default function home(){
                  <span className='absolute top-0 bottom-0 right-0 left-0 w-full h-screen bg-gradient-to-r from-slate-800/50 to-slate-800/50'></span>
                  {/* Header */}
                 <Header/>
+
+                {/* Home */}
+                 <div>
+                    <div>
+                        <h2>
+                            More than a watch — defining style
+                        </h2>
+                        <h3>
+                            Timeless beauty — always flawless
+                        </h3>
+                    </div>
+                    <div className=''>
+                        <Btn text='shop now'/>
+                        <Btn text='about us'/>
+                    </div>
+                 </div>
                  
             </div>
         </>
