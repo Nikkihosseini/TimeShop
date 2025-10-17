88c34d116dcac0ac94746578073aeaca0e4efce5
Nikkihosseini <niekal00@gmail.com>
2025-10-17 11:31:28 +0330

    style: update header and home section styles

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -4,7 +4,9 @@ import Header from '../component/Header'
 export default function home(){
     return(
         <>
-            <div className='w-full h-screen bg-home-desktop aspect-[2/1] bg-no-repeat bg-cover bg-[center-top]'>
+            <div className='relative w-full h-screen bg-home-desktop aspect-[2/1] bg-no-repeat bg-cover bg-[center-top] z-40'>
+                 <span className='absolute top-0 bottom-0 right-0 left-0 w-full h-screen bg-gradient-to-r from-slate-800/50 to-slate-800/50'></span>
+                 {/* Header */}
                 <Header/>
             </div>
         </>
