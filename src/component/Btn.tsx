c7cead40bb0810f4f16b0a6bc1197cc212968b80
Nikkihosseini <niekal00@gmail.com>
2025-10-18 19:33:35 +0330

    style: add styles for contact and home buttons

--- src/component/Btn.tsx
+++ src/component/Btn.tsx
@@ -1,7 +1,7 @@
 import type  { ButtonProps } from './types/types'
 
 
-export default function Btn({text}: ButtonProps){
+export default function Btn({ text }: ButtonProps){
     return(
         <>
             <div className='flex items-center justify-center h-[2.7rem] w-40 rounded-[2.5rem] border-2 border-amber-400 p-2 text-amber-400 font-semibold uppercase text-sm cursor-pointer'>
