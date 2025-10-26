73dc6cbbe71d76690336a4c1557da86576e97511
Nikkihosseini <niekal00@gmail.com>
2025-10-26 14:56:59 +0330

    feat: add product logic without styling

--- src/component/Product.tsx
+++ src/component/Product.tsx
@@ -1,13 +1,42 @@
+import type { Product } from './types/types'
 
 
 
 
 
-
-export default function Product(){
+export default function Product({id,image,name,price,prePrice} :Product){
     return(
         <>
-            
+            <div id={id}>
+                <div>
+                    <img src={image} alt="product-img"/>
+                <div>
+                    <div>
+                        <span className="material-symbols-outlined">visibility</span>
+                    </div>
+                    <div>
+                        <span className="material-symbols-outlined">shopping_cart</span>
+                    </div>
+                    <div>
+                        <span className="material-symbols-outlined">favorite</span>
+                    </div>
+                </div>
+                </div>
+
+                <div>
+                    <p>
+                        {name}
+                    </p>
+                    <div>
+                        <span>
+                            {price}
+                        </span>
+                        <span>
+                            {prePrice}
+                        </span>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
--- src/component/types/types.ts
+++ src/component/types/types.ts
@@ -4,3 +4,14 @@ export interface ButtonProps {
   text: string;
   style?: CSSProperties;
 }
+
+export interface Product {
+  id: string;
+  name: string;
+  price: number;
+  category: string;
+  inStock: boolean;
+  image: string;
+  prePrice?: number;
+  isLiked: boolean;
+}
