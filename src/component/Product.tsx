190b794a732fdc52bfcc0b01a987e3f02dd9cce0
Nikkihosseini <niekal00@gmail.com>
2025-11-02 19:10:29 +0330

    feat: show previous price only when available

--- src/component/Product.tsx
+++ src/component/Product.tsx
@@ -7,10 +7,10 @@ import type { ProductType  } from '../types/types'
 export default function Product({id,image,name,price,prePrice} :ProductType){
     return(
         <>
-            <div id={id} className='h-auto w-[330px]'>
-                <div className='h-[310px] w-[310px]'>
+            <div id={id} className='flex items-center justify-center flex-col h-auto w-[330px]'>
+                <div className='relative h-[310px] w-[310px]'>
                     <img className='h-full w-full' src={image} alt="product-img"/>
-                <div>
+                <div className='absolute top-1/3 right-0 my-auto'>
                     <div>
                         <span className="material-symbols-outlined">visibility</span>
                     </div>
@@ -23,16 +23,18 @@ export default function Product({id,image,name,price,prePrice} :ProductType){
                 </div>
                 </div>
 
-                <div>
+                <div className='flex justify-center items-center flex-col gap-5 mt-5'>
                     <p>
                         {name}
                     </p>
-                    <div>
+                    <div className='flex justify-between items-center gap-5'>
+                        {prePrice && (
+                            <span>
+                              ${prePrice}
+                            </span>
+                        )}
                         <span>
-                            {price}
-                        </span>
-                        <span>
-                            {prePrice}
+                            ${price}
                         </span>
                     </div>
                 </div>
